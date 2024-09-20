ba85b9425f265b1f06c965bf3036beac6fccb6f8
nandanaraju <nandanav0074@gmail.com>
2024-09-20 23:39:33 +0530

    modified

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     alchemyhol:{
       url: "https://eth-sepolia.g.alchemy.com/v2/oMJ_sGNdY_YrqfqVCisWV7_g-8JROIqu",
-      accounts:["2f81fbd962b14cab7ea4714dcec9c5b7bc3885c8f2083047fc3aec1808ec8876"]
+      accounts:[process.env.ALCHEMYHOL_PRIVATE_KEY_1]
     },
     hardhat: {
       // See its defaults
